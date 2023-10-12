25aea5ad9f1a52020076b0bed739bd59a31f3bc1
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-12 17:20:12 +0900

    Changed a module from fastify to express.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, {
-  //   logger: ['error', 'warn', 'log'],
-  //   cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
-  // });
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+    cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
+  });
 
   app.enableShutdownHooks();
 
