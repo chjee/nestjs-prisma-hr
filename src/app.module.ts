4bb9ae6079a24300ca5823978b5d338925184b7b
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-12 18:15:40 +0900

    Deleted incorrect phrases.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { EmployeeService } from './employee/employee.service';
 import { EmployeeModule } from './employee/employee.module';
-import { EmployeeController } from './employee/employee.controller';
-import { DepartmentService } from './department/department.service';
 import { DepartmentModule } from './department/department.module';
-import { DepartmentController } from './department/department.controller';
 
 @Module({
-  imports: [PrismaModule, EmployeeModule, DepartmentModule],
-  controllers: [AppController, EmployeeController, DepartmentController],
-  providers: [AppService, EmployeeService, DepartmentService],
+  imports: [EmployeeModule, DepartmentModule],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrismaService) {}
   getHello(): string {
     return 'Hello World!';
   }
