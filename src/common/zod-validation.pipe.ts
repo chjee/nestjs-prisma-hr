1a61d9645a21bd614c1f1eb15f18a0164a03749c
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-13 20:39:31 +0900

    Added a zod module & function.

---
+++ src/common/zod-validation.pipe.ts
@@ -0,0 +1,20 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { ZodObject } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodObject<any>) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      this.schema.parse(value);
+    } catch (error) {
+      throw new BadRequestException('Validation failed');
+    }
+    return value;
+  }
+}
