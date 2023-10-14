c7fe09f795ad809b471264b98020b7be4f5647a8
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-14 15:15:55 +0900

    Coordinated some files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+export const parser = '@typescript-eslint/parser';
+export const parserOptions = {
+  project: 'tsconfig.json',
+  tsconfigRootDir: __dirname,
+  sourceType: 'module',
+};
+export const plugins = ['@typescript-eslint/eslint-plugin'];
+export const extendsArr = [
+  'plugin:@typescript-eslint/recommended',
+  'plugin:prettier/recommended',
+];
+export const root = true;
+export const env = {
+  node: true,
+  jest: true,
+};
+export const ignorePatterns = ['.eslintrc.js'];
+export const rules = {
+  '@typescript-eslint/interface-name-prefix': 'off',
+  '@typescript-eslint/explicit-function-return-type': 'off',
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
 };
