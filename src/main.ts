25385b99c0493a033beecec843675ed8b23fe216
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-15 17:35:34 +0900

    Added a swagger modules.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
     cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Oracle HR Example')
+    .setDescription('The HR API description')
+    .setVersion('1.0')
+    .addTag('HR')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableShutdownHooks();
 
